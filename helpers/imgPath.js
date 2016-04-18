c669d3a3a57bcc7ba4ae21f7e63810289ca6c627
Dmytro Sopin <Dmytro_Sopin@epam.com>
2016-04-18 18:05:46 +0300

    added helper for img paths

---
+++ helpers/imgPath.js
@@ -0,0 +1,14 @@
+const SOURCE = "images/";
+
+module.exports = function (obj, field) {
+
+    if(!field) {
+        field = 'src';
+    }
+
+    for (var i = 0, length = obj.length; i < length; i++) {
+        obj[i][field] = SOURCE + obj[i][field];
+    }
+
+    return obj;
+};
