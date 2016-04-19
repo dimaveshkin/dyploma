b43baf67aa11b4f1f2d90fab8ce187e975bf6458
Dmytro_Veshkin <dmytro_veshkin@epam.com>
2016-04-19 11:43:52 +0300

    Merge branch 'master' of https://github.com/dimaveshkin/dyploma
    # Conflicts:
    #	app.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 var router = express.Router();
 var galleryAPI = require('./gallery');
 var socialsAPI = require('./socials');
+var feedbackAPI = require('./feedback');
 
 router.use("/gallery", galleryAPI);
 router.use("/socials", socialsAPI);
+router.use("/feedback", feedbackAPI);
 
 module.exports = router; 
