2f05f271ce6c6505e629cde618d1a783a7c76560
Dmytro Sopin <Dmytro_Sopin@epam.com>
2016-04-20 18:37:10 +0300

    started api for tours

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var galleryAPI = require('./gallery');
 var socialsAPI = require('./socials');
 var feedbackAPI = require('./feedback');
+var toursAPI = require('./tours');
 
 router.use("/gallery", galleryAPI);
 router.use("/socials", socialsAPI);
 router.use("/feedback", feedbackAPI);
+router.use("/tours", toursAPI);
 
 module.exports = router; 
