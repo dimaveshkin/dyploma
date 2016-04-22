fb4fda9441fe1509d051a77976f887b72ac61248
Dmytro_Veshkin <dmytro_veshkin@epam.com>
2016-04-22 13:48:01 +0300

    tours

--- public/src/js/view/NextToursView.js
+++ public/src/js/view/NextToursView.js
@@ -1,4 +1,5 @@
 var Backbone = require("backbone");
+var _ = require("underscore");
 Backbone.$ = window.$;
 
 var nextToursViewTmp = require("./templates/NextToursViewTmp.hbs");
@@ -7,10 +8,33 @@ var NextToursView = Backbone.View.extend({
     el: ".next-tours",
     template: nextToursViewTmp,
     initialize: function () {
-
+        this.slidersOpt = {
+            minSlides: 3,
+            maxSlides: 3,
+            slideMargin: 16,
+            slideWidth: 1000,
+            pager: false,
+            responsive: true,
+            adaptiveHeight: true
+        };
     },
     render: function (){
-        this.$el.html(this.template());
+        var that = this;
+
+        $.get("/api/tours/next", function (tours) {
+            var years = _.map(tours, function (tour) {
+                var date = new Date(tour.startDate);
+
+                return date.getFullYear();
+            });
+
+            years = _.uniq(years);
+            years = years.sort();
+
+            that.$el.html(that.template({years: years, tours: tours}));
+
+            $(".next-tours-gallery").bxSlider(that.slidersOpt);
+        });
     }
 });
 
