4362e2cfdee815a28bad1f91665cc41007ba8cb1
Dmytro Sopin <Dmytro_Sopin@epam.com>
2016-04-23 17:16:58 +0300

    added jquery

--- admin_public/src/js/routers/AppRouter.js
+++ admin_public/src/js/routers/AppRouter.js
@@ -3,28 +3,13 @@ var _ = require("underscore");
 Backbone.$ = window.$;
 
 var AppView = require("../view/AppView");
-//var GalleryPageView = require("../view/GalleryPageView");
 var MainPageView = require("../view/MainPageView");
 var ContactsPageView = require("../view/ContactsPageView");
-//var NotFoundPageView = require("../view/NotFoundPageView");
-//var AboutPageView = require("../view/AboutPageView");
-//var CountryGalleryPageView = require("../view/CountryGalleryPageView");
-//var TourPageView = require("../view/TourPageView");
-//var ToursPageView = require("../view/ToursPageView");
-//var LoginPageView = require("../view/LoginPageView");
-//var socials = require("../models/SocialsModel");
 
 var Router = Backbone.Router.extend({
   routes: {
     "": "index",
     "contacts": "contacts"
-//        "tours": "tours",
-//        "about": "about",
-//        "gallery/:name": "countryGallery",
-//        "login": "login",
-//        "admin/password": "adminPassword",
-//        "tours/:id": "tourInfo",
-//        "*NotFound": "notFound"
   },
   initialize: function (options) {
     _.bindAll(this, "index");
--- admin_public/src/js/view/AppView.js
+++ admin_public/src/js/view/AppView.js
@@ -1,6 +1,5 @@
 var Backbone = require("backbone");
 var _ = require("underscore");
-var EventBus = require("../helpers/EventBus");
 Backbone.$ = window.$;
 
 var appTmp = require("./templates/AppViewTmp.hbs");
